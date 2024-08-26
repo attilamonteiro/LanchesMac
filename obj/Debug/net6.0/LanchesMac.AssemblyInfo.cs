c51b3af889e75a7e396052815bea848179b1385e
Attila <attila.abreu@leste.com>
2024-08-26 14:20:37 -0300

    Atualiza strings de conexão e versão do assembly
    No `appsettings.json`, a string de conexão padrão foi alterada para um novo servidor e uma nova string de conexão chamada `SecondaryConnection` foi adicionada.

--- obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
+++ obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9010dcc571639d882cd112a82a241f91c6e4444d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
